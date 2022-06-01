1e207e5d18c1a50146a9759c3a2641eb6669fcd9
adryell <adryell.silva10@gmail.com>
2022-05-31 22:52:21 -0300

    create permission migration

--- database/migrations/2022_06_01_012245_create_permission_table.php
+++ database/migrations/2022_06_01_012245_create_permission_table.php
@@ -13,15 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('permission', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->unsignedBigInteger('permission_id');
-            $table->rememberToken();
-            $table->timestamps();
+        });
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('permission_id')->references('id')->on('permission');
         });
     }
 
@@ -32,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('permission');
     }
 };
